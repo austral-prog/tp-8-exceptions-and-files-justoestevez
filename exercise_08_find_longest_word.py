77e72102c709cd75cb28210c028af61247669d71
justoestevez <jeestevez@mail.austral.edu.ar>
2026-05-18 20:10:01 -0300

    Update exercise_08_find_longest_word.py

--- exercise_08_find_longest_word.py
+++ exercise_08_find_longest_word.py
@@ -27,4 +27,27 @@ def find_longest_word(filename):
         # archivo contiene: "el gato corre rapido\npor el jardin\n"
         find_longest_word("texto.txt") -> "rapido"
     """
-    pass  # Reemplazar con tu implementación
+palabra_ganadora = ""
+    max_letras = -1  # Empezamos con un piso bien bajo
+    
+    # 1. Abrimos y leemos todo el archivo
+    with open(filename, 'r') as archivo:
+        contenido = archivo.read()
+        
+        # 2. Pasamos la tijera para separar por espacios y saltos de línea
+        lista_palabras = contenido.split() 
+        # Si el archivo decía "el gato corre", lista_palabras es ['el', 'gato', 'corre']
+        
+    # 3. El palito del test: Si el archivo estaba vacío, la lista queda []
+    if len(lista_palabras) == 0:
+        raise ValueError("file has no words") # Plantamos bandera de error
+        
+    # 4. El bucle para buscar la más larga (Como buscar el mayor puntaje)
+    for palabra in lista_palabras:
+        
+        # len(palabra) nos dice cuántas letras tiene la palabra actual
+        if len(palabra) > max_letras:
+            max_letras = len(palabra)  # Guardamos el nuevo récord de letras
+            palabra_ganadora = palabra # Guardamos la palabra que rompió el récord
+            
+    return palabra_ganadora

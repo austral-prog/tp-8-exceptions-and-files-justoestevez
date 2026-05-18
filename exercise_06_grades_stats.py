3561523c391f34c08f69460cb88ed3b1abd8f94b
justoestevez <jeestevez@mail.austral.edu.ar>
2026-05-18 19:27:00 -0300

    Update exercise_06_grades_stats.py

--- exercise_06_grades_stats.py
+++ exercise_06_grades_stats.py
@@ -34,4 +34,37 @@ def grades_stats(filename):
             "Cami": (10.0, 10.0, 10.0),
         }
     """
-    pass  # Reemplazar con tu implementación
+    resultado_final = {}
+    
+    with open(filename, 'r') as archivo:
+        for linea in archivo:
+            linea_limpia = linea.strip()
+            
+            # Si la línea tiene datos (no está vacía)
+            if linea_limpia: 
+                
+                # PRIMER CORTE: Separamos el nombre del "choclo" de notas
+                nombre, bloque_notas = linea_limpia.split(':')
+                # nombre = 'Ana'
+                # bloque_notas = '8,9,7'
+                # quedaria una lista la cual es ['Ana', '8,9,7']
+                # SEGUNDO CORTE: Cortamos las notas por las comas
+                lista_notas_texto = bloque_notas.split(',')  #solo se lo hace a las notas, el nombre queda
+                # lista_notas_texto = ['8', '9', '7']
+                
+                # PASAR A NÚMEROS: Como están como texto, las pasamos a float
+                lista_notas_numeros = []
+                for nota_txt in lista_notas_texto:
+                    lista_notas_numeros.append(float(nota_txt))
+                # lista_notas_numeros = [8.0, 9.0, 7.0]
+                
+                # LOS TRES CÁLCULOS:
+                promedio = sum(lista_notas_numeros) / len(lista_notas_numeros)
+                maximo = max(lista_notas_numeros)
+                minimo = min(lista_notas_numeros)
+                
+                # GUARDAR EN EL DICCIONARIO:
+                # La clave es el nombre, y el valor es la tupla (promedio, max, min)
+                resultado_final[nombre] = (promedio, maximo, minimo)
+                
+    return resultado_final

daca72595af85c996e7f1f7675d298af79da8851
justoestevez <jeestevez@mail.austral.edu.ar>
2026-05-18 22:20:25 -0300

    Update exercise_09_merge_files.py

--- exercise_09_merge_files.py
+++ exercise_09_merge_files.py
@@ -32,4 +32,14 @@ def merge_files(file1, file2, output):
         # hola
         # mundo
     """
-    pass  # Reemplazar con tu implementación
+    with open(file1, 'r') as archivo1:
+        contenido1 = archivo1.read()
+        
+    # 2. Leemos el segundo archivo de forma segura
+    with open(file2, 'r') as archivo2:
+        contenido2 = archivo2.read()
+        
+    # 3. Recién si los dos de arriba existían, creamos el archivo de salida
+    with open(output, 'w') as archivo_salida:
+        # Sumamos los textos (los concatenamos) y los escribimos
+        archivo_salida.write(contenido1 + contenido2)
